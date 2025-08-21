99044557acd5990339b6a1764fa5248e34da5491
samuel habtamu <152323187+Samuelgit3919@users.noreply.github.com>
2025-08-21 15:15:57 +0300

    enhanced some code

--- src/components/Dashboard/Cashier/Cashier.jsx
+++ src/components/Dashboard/Cashier/Cashier.jsx
@@ -262,7 +262,6 @@ export default function Cashier() {
                                         </div>
                                     )
                                 }
-
                             </div>
                         </>
                     )}
